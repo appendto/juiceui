8ec0d72a95d4dcde27e4dbdadeebfab15e0fc61b
shellscape <andrew@shellscape.org>
2012-03-12 23:37:08 -0400

    adding jquery mobile app class

---
+++ Juice/Mobile/JuiceMobileApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.UI;
+
+using Juice.Framework;
+
+namespace Juice.Mobile {
+	public static class JuiceMobileApp {
+
+		public static void Start() {
+
+			ScriptManager.ScriptResourceMapping.AddDefinition("jquery-mobile",
+					new ScriptResourceDefinition {
+						Path = "~/Scripts/jquery.mobile-1.0.1.min.js",
+						DebugPath = "~/Scripts/jquery.mobile-1.0.1.js",
+						CdnPath = "http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js",
+						CdnDebugPath = "http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.js",
+						CdnSupportsSecureConnection = true
+					}
+			);
+
+			CssManager.CssResourceMapping.AddDefinition("jquery-mobile", new CssResourceDefinition {
+				Path = "~/Content/jquery.mobile-1.0.1.min.css",
+				DebugPath = "~/Content/jquery.mobile-1.0.1.css",
+				CdnPath = "http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css",
+				CdnDebugPath = "http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.css"
+			});
+
+		}
+	}
+}
