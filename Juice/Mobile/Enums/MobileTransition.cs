d2b0c2d5b4a35fccc07d3c9752ce0d9a32a30624
shellscape <andrew@shellscape.org>
2012-03-14 01:15:11 -0400

    adding mobile enums

---
+++ Juice/Mobile/Enums/MobileTransition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Juice.Mobile {
+	public enum MobileTransition {
+		None,
+		Slide,
+		SlideUp,
+		SlideDown,
+		Pop,
+		Fade,
+		Flip
+	}
+}
