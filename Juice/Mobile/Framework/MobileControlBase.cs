82553e9a6786b7de2c2eb3315919d14416284f55
shellscape <andrew@shellscape.org>
2012-03-14 01:15:33 -0400

    adding supporting mobile framework classes

---
+++ Juice/Mobile/Framework/MobileControlBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Juice.Mobile.Framework {
+	public abstract class MobileControlBase : WebControl {
+
+		public MobileControlBase(String role) {
+			this.Role = role;
+			this.ClientIDMode = ClientIDMode.Static;
+		}
+
+		protected override HtmlTextWriterTag TagKey { get { return HtmlTextWriterTag.Div; } }
+
+		public String Role { get; protected set; }
+
+		protected override void OnPreRender(EventArgs e) {
+
+			if(this.Role != null) {
+				this.Attributes["data-role"] = this.Role;
+			}
+
+			// TODO: parse the options, compare to the default, add attributes if necessary.
+			
+			base.OnPreRender(e);
+		}
+	}
+}
---
+++ Juice/Mobile/Framework/MobileExtender.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Juice.Framework;
+
+namespace Juice.Mobile.Framework {
+	public class MobileExtender : ExtenderControl {
+
+		private Control _targetControl;
+
+		public MobileExtender() : this(null) { }
+
+		public MobileExtender(String role) {
+			this.Role = role;
+			this.ClientIDMode = ClientIDMode.Static;
+		}
+
+		internal Control TargetControl { get; private set; }
+
+		//data-mini	true | false - Compact sized version
+		[WidgetOption("mini", false)]
+		public Boolean Mini { get; set; }
+
+		[WidgetOption("role", null)]
+		public String Role { get; protected set; }
+
+		//data-theme	swatch letter (a-z)
+		[WidgetOption("theme", null)]
+		public String Theme { get; set; }
+
+		protected override void OnLoad(EventArgs e) {
+			base.OnLoad(e);
+
+			_targetControl = FindControl(TargetControlID);
+
+			if(_targetControl == null) {
+				throw new ArgumentNullException("TargetControl is null");
+			}
+
+			this.TargetControl = _targetControl;
+		}
+
+		protected override void OnPreRender(EventArgs e) {
+			
+			// TODO: parse the options, compare to the default, add attributes if necessary.
+			
+			base.OnPreRender(e);
+		}
+
+	}
+}
