57414bf46be5ccce01be9650a4c30d177535fd52
shellscape <andrew@shellscape.org>
2012-10-17 10:57:59 -0400

    merge stashed changes for menu

--- Juice/Menu.cs
+++ Juice/Menu.cs
@@ -26,6 +26,10 @@ namespace Juice {
 	}
 
 	[ParseChildren(typeof(MenuItem), DefaultProperty = "Items", ChildrenAsProperties = true)]
+	[WidgetEvent("blur")]
+	[WidgetEvent("create")]
+	[WidgetEvent("focus")]
+	[WidgetEvent("select")]
 	public class Menu : JuiceScriptControl, IAutoPostBackWidget {
 
 		public Menu() : base("menu") {
@@ -39,7 +43,47 @@ namespace Juice {
 
 		#region Widget Options
 
+		/// <summary>
+		/// Icons to use for submenus, matching an icon defined by the jQuery UI CSS Framework.
+		/// Reference: http://api.jqueryui.com/menu/#option-icons
+		/// </summary>
+		[WidgetOption("icons", "{ submenu: \"ui-icon-carat-1-e\" }", Eval = true)]
+		[TypeConverter(typeof(Framework.TypeConverters.JsonObjectConverter))]
+		[Category("Appearance")]
+		[DefaultValue("{ submenu: \"ui-icon-carat-1-e\" }")]
+		[Description("Icons to use for submenus, matching an icon defined by the jQuery UI CSS Framework.")]
+		public string Icons { get; set; }
 
+		/// <summary>
+		/// Selector for the elements that serve as the menu container, including sub-menus.
+		/// Reference: http://api.jqueryui.com/menu/#option-menu
+		/// </summary>
+		[WidgetOption("menu", "ul")]
+		[Category("Behavior")]
+		[DefaultValue("ul")]
+		[Description("Selector for the elements that serve as the menu container, including sub-menus.")]
+		public string MenuSelector { get; set; }
+
+		/// <summary>
+		/// Identifies the position of submenus in relation to the associated parent menu item. The of option defaults to the parent menu item, but you can specify another element to position against. You can refer to the jQuery UI Position utility for more details about the various options.
+		/// Reference: http://api.jqueryui.com/menu/#option-position
+		/// </summary>
+		[WidgetOption("position", "{ my: \"top left\", at: \"top right\" }", Eval = true)]
+		[TypeConverter(typeof(JsonObjectConverter))]
+		[Category("Layout")]
+		[DefaultValue("{ my: \"top left\", at: \"top right\" }")]
+		[Description("Identifies the position of submenus in relation to the associated parent menu item. The of option defaults to the parent menu item, but you can specify another element to position against. You can refer to the jQuery UI Position utility for more details about the various options.")]
+		public string Position { get; set; }
+
+		/// <summary>
+		/// Customize the ARIA roles used for the menu and menu items. The default uses "menuitem" for items. Setting the role option to "listbox" will use "option" for items. If set to null, no roles will be set, which is useful if the menu is being controlled by another element that is maintaining focus.
+		/// Reference: http://api.jqueryui.com/menu/#option-menu
+		/// </summary>
+		[WidgetOption("role", "menu")]
+		[Category("Behavior")]
+		[DefaultValue("menu")]
+		[Description("Customize the ARIA roles used for the menu and menu items. The default uses \"menuitem\" for items. Setting the role option to \"listbox\" will use \"option\" for items. If set to null, no roles will be set, which is useful if the menu is being controlled by another element that is maintaining focus.")]
+		public string Role { get; set; }
 
 		#endregion
 
