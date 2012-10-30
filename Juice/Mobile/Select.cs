47a6aadf960436bd676854ba5998051cdce06e9e
shellscape <andrew@shellscape.org>
2012-10-30 14:47:19 -0400

    updating mobile control attributes: select, slider

--- Juice/Mobile/Select.cs
+++ Juice/Mobile/Select.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Web.UI;
@@ -16,27 +17,69 @@ namespace Juice.Mobile {
 	public class Select : Juice.Mobile.Framework.MobileExtender {
 
 		//data-icon	home | delete | plus | arrow-u | arrow-d | check | gear | grid | star | custom | arrow-r | arrow-l | minus | refresh | forward | back | alert | info | search
+		/// <summary>
+		/// Applies an icon from the icon set.
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("icon", null)]
+		[Category("Appearance")]
+		[DefaultValue(null)]
+		[Description("Applies an icon from the icon set.")]
 		public MobileIcon? Icon { get; set; }
 
 		//data-iconpos	left | right | top | bottom | notext
+		/// <summary>
+		/// Positions the icon in the button. Possible values: left, right, top, bottom, none, notext. The notext value will display an icon-only button with no text feedback.
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("iconpos", null)]
+		[Category("Appearance")]
+		[DefaultValue(null)]
+		[Description("Positions the icon in the button. Possible values: left, right, top, bottom, none, notext. The notext value will display an icon-only button with no text feedback.")]
 		public MobileIconPosition? IconPosition { get; set; }
 		
 		//data-inline	true | false
+		/// <summary>
+		/// Display the control inline, horizontally.
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("inline", false)]
+		[Category("Appearance")]
+		[DefaultValue(false)]
+		[Description("Display the control inline, horizontally.")]
 		public Boolean Inline { get; set; }
 
 		//data-native-menu	true | false
+		/// <summary>
+		/// Display the native OS menu.
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("native-menu", true)]
+		[Category("Behavior")]
+		[DefaultValue(true)]
+		[Description("Display the native OS menu.")]
 		public Boolean UseNativeMenu { get; set; }
 
 		//data-overlay-theme	swatch letter (a-z) - overlay theme when the page is opened in a dialog
+		/// <summary>
+		/// Overlay theme for non-native selects
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("overlay-theme", null)]
+		[Category("Appearance")]
+		[DefaultValue(null)]
+		[Description("Overlay theme for non-native selects")]
 		public String OverlayTheme { get; set; }
 
 		//data-placeholder	true | false - Add to the Option
+		/// <summary>
+		/// Can be set on option element of a non-native select
+		/// Reference: http://jquerymobile.com/demos/1.2.0/docs/api/data-attributes.html
+		/// </summary>
 		[WidgetOption("placeholder", false)]
+		[Category("Appearance")]
+		[DefaultValue(false)]
+		[Description("Can be set on option element of a non-native select")]
 		public Boolean Placeholder { get; set; }
 	}
 }
