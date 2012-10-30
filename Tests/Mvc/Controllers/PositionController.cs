a158ae2ce7ed649d0b375a6214c7aa80a1d7968f
shellscape <andrew@shellscape.org>
2012-10-30 13:15:03 -0400

    adding mvc tests for droppable, position, resizable, selectable, sortable, draggable

---
+++ Tests/Mvc/Controllers/PositionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class PositionController : Controller
+    {
+        //
+        // GET: /Position/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Tests/Mvc/Controllers/SortableController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class SortableController : Controller
+    {
+        //
+        // GET: /Sortable/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
