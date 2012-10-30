91432f10cc61c85a94e34b211f190de2373cd2c2
shellscape <andrew@shellscape.org>
2012-10-30 03:09:32 -0400

    initial mvc tests commit

---
+++ Tests/Mvc/Controllers/ProgressbarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class ProgressbarController : Controller
+    {
+        //
+        // GET: /Progressbar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
