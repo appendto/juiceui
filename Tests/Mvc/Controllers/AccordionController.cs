62c95f53b210751ad689bb96d42d46eadc187f0f
shellscape <andrew@shellscape.org>
2012-10-30 12:43:18 -0400

    adding mvc test pages

---
+++ Tests/Mvc/Controllers/AccordionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class AccordionController : Controller
+    {
+        //
+        // GET: /Accordion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
