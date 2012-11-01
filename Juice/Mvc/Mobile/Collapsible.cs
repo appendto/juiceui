b0e16f9f4b1a5e3750316729bf61673bf517bd32
shellscape <andrew@shellscape.org>
2012-11-01 12:25:11 -0400

    updates for mobile mvc collapsibles tests

--- Juice/Mvc/Mobile/Collapsible.cs
+++ Juice/Mvc/Mobile/Collapsible.cs
@@ -19,11 +19,11 @@ namespace Juice.Mvc.Mobile {
 		/// <param name="mini">Sets the size of the element to a more compact, mini version.</param>,
 		/// <param name="theme">Defines the theme swatch letter (a-z)</param>
 		/// <returns>CollapsibleWidget</returns>
-		public CollapsibleWidget BeginCollapsible(String elementId = "", Boolean collapsed = true, String contentTheme = null, Boolean mini = false, String theme = null) {
+		public CollapsibleWidget BeginCollapsible(String elementId = "", Boolean collapsed = true, MobileIcon? collapsedIcon = null, String contentTheme = null, MobileIcon? expandedIcon = null, MobileIconPosition? iconpos = null, Boolean inset = true, Boolean mini = false, String theme = null) {
 			var widget = new CollapsibleWidget(_helper);
 
 			widget.SetCoreOptions(elementId, null);
-			widget.Options(collapsed, contentTheme, mini, theme);
+			widget.Options(collapsed, collapsedIcon, contentTheme, expandedIcon, iconpos, inset, mini, theme);
 
 			return widget;
 		}
@@ -38,19 +38,27 @@ namespace Juice.Mvc.Mobile {
 
 	public class CollapsibleWidget : JuiceMobileWidget<CollapsibleWidget>, IDisposable {
 
-		public CollapsibleWidget(HtmlHelper helper) : base(helper, "collapsable") {
+		public CollapsibleWidget(HtmlHelper helper) : base(helper, "collapsible") {
 			_optionsMap = new Dictionary<String, String> {
 				{ "collapsed", "collapsed" },
+				{ "collapsedIcon", "collapsed-icon" },
 				{ "contentTheme", "content-theme" },
+				{ "expandedIcon", "expanded-icon" },
+				{ "iconpos", "iconpos" },
+				{ "inset", "inset" },
 				{ "mini", "mini" },
 				{ "theme", "theme" }
 			};
 		}
 
-		public CollapsibleWidget Options(Boolean collapsed = true, String contentTheme = null, Boolean mini = false, String theme = null) {
+		public CollapsibleWidget Options(Boolean collapsed = true, MobileIcon? collapsedIcon = null, String contentTheme = null, MobileIcon? expandedIcon = null, MobileIconPosition? iconpos = null, Boolean inset = true, Boolean mini = false, String theme = null) {
 			base.SetOptions(
 				JuiceHelpers.GetMemberInfo(() => collapsed),
+				JuiceHelpers.GetMemberInfo(() => collapsedIcon),
 				JuiceHelpers.GetMemberInfo(() => contentTheme),
+				JuiceHelpers.GetMemberInfo(() => expandedIcon),
+				JuiceHelpers.GetMemberInfo(() => iconpos),
+				JuiceHelpers.GetMemberInfo(() => inset),
 				JuiceHelpers.GetMemberInfo(() => mini),
 				JuiceHelpers.GetMemberInfo(() => theme)
 			);
